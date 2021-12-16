320117291d7f1b101822bf675d0cd7fa1cd4ec31
TheodoreFrazier <theo.frazier@icloud.com>
2021-12-16 12:07:29 -0500

    moved the navbar links into a Nav tag to pass tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,13 @@ import "./NavBar.css"
 class NavBar extends React.Component {
     render() {
         return (
-            <div class="nav-bar">
+            <div >
+<nav class="nav-bar">
                 <Link to="/">Home</Link>
                 <Link to="/movies">Movies</Link>
                 <Link to="/people">People</Link>
                 <Link to="/locations">Locations</Link>
+                </nav>
             </div>
         )
     }
