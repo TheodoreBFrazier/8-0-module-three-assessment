0aa37f7658f03319ac0a43392dbc47ef2303df41
TheodoreFrazier <theo.frazier@icloud.com>
2021-12-16 11:44:53 -0500

    Added links to NavBar - working. Added Routes to App.js and fixed Navbar import in line 6 so that path matches the format in the component folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import "./App.css";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
-    </div>
-  );
+import {Switch, Route} from 'react-router-dom'
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Locations from './components/Locations';
+import Movies from './components/Movies';
+import People from './components/People';
+import Home from './components/Home';
+
+class App extends React.Component  {
+
+  render() {
+
+    return (
+      <div className="App">
+       <NavBar />
+        <Switch>
+          <Route exact path = '/' component = {Home} />
+          <Route path = '/locations' component = {Locations} />
+          <Route path = '/movies' component = {Movies} />
+          <Route path = '/people' component = {People} />
+        </Switch>
+      </div>
+    );
+
+  }
+
 }
+
+
 
 export default App;
